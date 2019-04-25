fda80a5acb70107ea16e204b838e0075d799d08f
Hiroki Kinoshita <hiroki.kinoshita@sun-asterisk.com>
2019-04-25 17:53:27 +0900

    PJ検索機能 #19

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,12 @@
 class Project < ApplicationRecord
   has_many :members_projects
   has_many :members, through: :members_projects
+
+  def self.search(search) #self.でクラスメソッドとしている
+    if search # Controllerから渡されたパラメータが!= nilの場合は、titleカラムを部分一致検索
+      Project.where(['name LIKE ?', "%#{search}%"])
+    else
+      Project.all #全て表示。
+    end
+  end
 end
