25c676e800f884b7a03a0b7bb90e06959e94da50
Hiroki Kinoshita <hiroki.kinoshita@sun-asterisk.com>
2019-04-25 21:18:28 +0900

    メンバー検索　修正

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,9 +2,9 @@ class Member < ApplicationRecord
   has_many :members_projects
   has_many :projects, through: :members_projects
 
-  def self.search(search) #self.でクラスメソッドとしている
-    if search # Controllerから渡されたパラメータが!= nilの場合は、titleカラムを部分一致検索
-      Member.where(['name LIKE ?', "%#{search}%"])
+  def self.search(keyword)
+    if keyword
+      Member.where('name LIKE ?', "%#{keyword}%")
     else
       Member.all #全て表示。
     end
--- app/models/project.rb
+++ app/models/project.rb
@@ -2,9 +2,9 @@ class Project < ApplicationRecord
   has_many :members_projects
   has_many :members, through: :members_projects
 
-  def self.search(search) #self.でクラスメソッドとしている
-    if search # Controllerから渡されたパラメータが!= nilの場合は、titleカラムを部分一致検索
-      Project.where(['name LIKE ?', "%#{search}%"])
+  def self.search(keyword) 
+    if keyword 
+      where('name LIKE ?', "%#{keyword}%")
     else
       Project.all #全て表示。
     end
